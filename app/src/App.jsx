47d6173252e65165a863a44becc2ee91f836435b
黄智军 <353038262@qq.com>
2025-11-19 20:56:10 +0800

    chore(tsconfig):调整了下别名范围

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
+import Markdown from '@/util/MarkScribe'
 
+
+function App() {
+  let md = new Markdown();
+  console.log(md.parse(`# GGG`))
+  let md2 = new Markdown();
+  console.log(md2.parse(`# GG2`))
   return (
     <>
       hello,world
